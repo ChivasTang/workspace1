3a8bfaf468d0270c6dc38a0c6d603a9b0c130ce2
湯志華 <tsk@tangzhihuadeMacBook-Pro.local>
2020-01-03 15:37:30 +0900

    20200103

---
+++ flyingStone/backStage/src/main/java/com/flyingStone/backStage/web/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.flyingStone.backStage.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public ModelAndView login(HttpServletRequest request, HttpServletResponse response){
+        return new ModelAndView("admin/login");
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public void login(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception{
+        String token="flyingStoneTokenSecretKey";
+
+    }
+}
